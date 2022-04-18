3902ac008f3a75b92bd95c078581debcc13ec938
zahidalidev <m.zahidalidev@gmail.com>
2022-04-18 06:19:48 +0500

    refactoring

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.listen(app.get("port"), () =>
   console.log(`app is running on port ${app.get("port")}...`)
 );
 
-// const con = require("./config/db");
+// const con = require("./config/db")
 
 // con.connect(function (err) {
 //   if (err) throw err;
