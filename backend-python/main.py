186e2784090f0d7092160f69770f3670de561f09
zahidalidev <m.zahidalidev@gmail.com>
2022-04-24 21:32:34 +0500

    formated

--- backend-python/main.py
+++ backend-python/main.py
@@ -3,18 +3,15 @@ from flask_restful import Api
 from flask_cors import CORS
 
 from resources.trainModel import TrainModel
-from resources.user import User
 from resources.verifyImages import VerifyImages
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-api.add_resource(TrainModel, '/trainmodel', '/trainmodel/<id>')
+api.add_resource(TrainModel, '/trainmodel')
 
 api.add_resource(VerifyImages, '/verifyimages')
-
-api.add_resource(User, '/user', '/user/<id>', '/user/<email>/<password>')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
