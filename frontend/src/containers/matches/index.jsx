eff2bd00be7c21a2749051b3ba5cba68a249b7c8
zahidalidev <m.zahidalidev@gmail.com>
2022-05-22 19:14:51 +0500

    footer in matches

--- frontend/src/containers/matches/index.jsx
+++ frontend/src/containers/matches/index.jsx
@@ -13,6 +13,7 @@ import { nodeBaseURL } from 'config/baseURL'
 
 import 'containers/matches/styles.css'
 import { ADD_CURRENT_PROFILE, ADD_MATCHED_PROFILE } from 'store/profiles'
+import Footer from 'components/footer/Footer'
 
 const Matches = () => {
   const { pathname } = useLocation()
@@ -78,78 +79,81 @@ const Matches = () => {
   console.log('currentprofileDetail: ', currentprofileDetail)
   console.log('matchedProfiles: ', matchedProfiles)
   return (
-    <div className='d-flex align-items-center justify-content-center home-container'>
-      <div className='d-flex flex-row col-md-10 align-items-start'>
-        <div className='d-flex flex-column col-md-3 justify-content-end align-items-center'>
-          <Card className='d-flex profile-card-h flex-column justify-content-center align-items-center'>
-            <img
-              className='profile-img-h'
-              src={`${nodeBaseURL}/${currentprofileDetail.image}`}
-              alt='Profile Image'
-            />
-            <h5 className='profile-heading-h'>{currentprofileDetail.name}</h5>
-            <p className='profile-title-h'>{currentprofileDetail.occupation}</p>
-            <span className='horizontal-c-barder' />
-            <h6 className='profile-completion'>
-              Perosnality Type: {currentprofileDetail.personality_type}
-            </h6>
-            <div className='btn-pro-h'>
-              <Button
-                onClick={() =>
-                  currentprofileDetail.plan === 'premium'
-                    ? toast.info('You are a Premium Member')
-                    : navigate('/subscription')
-                }
-                title={currentprofileDetail.plan === 'premium' ? 'Premium' : 'Upgrade Pro'}
-                width='15.5rem'
-                height='2.8rem'
+    <>
+      <div className='d-flex align-items-center justify-content-center home-container'>
+        <div className='d-flex flex-row col-md-10 align-items-start'>
+          <div className='d-flex flex-column col-md-3 justify-content-end align-items-center'>
+            <Card className='d-flex profile-card-h flex-column justify-content-center align-items-center'>
+              <img
+                className='profile-img-h'
+                src={`${nodeBaseURL}/${currentprofileDetail.image}`}
+                alt='Profile Image'
               />
-            </div>
-          </Card>
-          <div className='d-flex flex-column side-menues-h justify-content-center align-items-start'>
-            {sideBarHomeMenues.map(item => (
-              <div
-                onClick={() => navigate(item.path)}
-                className={`d-flex ${
-                  pathname === item.path ? 'menu-container-active' : null
-                } menu-container-h flex-row justify-content-start align-items-center`}
-                key={item.title}
-              >
-                {item.icon}
-                <h6 className='menu-heading-h'>{item.title}</h6>
+              <h5 className='profile-heading-h'>{currentprofileDetail.name}</h5>
+              <p className='profile-title-h'>{currentprofileDetail.occupation}</p>
+              <span className='horizontal-c-barder' />
+              <h6 className='profile-completion'>
+                Perosnality Type: {currentprofileDetail.personality_type}
+              </h6>
+              <div className='btn-pro-h'>
+                <Button
+                  onClick={() =>
+                    currentprofileDetail.plan === 'premium'
+                      ? toast.info('You are a Premium Member')
+                      : navigate('/subscription')
+                  }
+                  title={currentprofileDetail.plan === 'premium' ? 'Premium' : 'Upgrade Pro'}
+                  width='15.5rem'
+                  height='2.8rem'
+                />
               </div>
-            ))}
-          </div>
-        </div>
-        <div className='d-flex flex-column col-md-9'>
-          <div className='d-flex flex-column new-matches-h p-4'>
-            <div className='d-flex flex-row align-items-center'>
-              <div className='d-flex flex-column'>
-                <h5 className='recent-heading-h'>Matches</h5>
-                <h5 className='new-match-heading-h'>
-                  PAAKMATCH, Found {matchedProfiles.length} Members{' '}
-                </h5>
-              </div>
-            </div>
-            <div className='d-flex flex-row recent-h-container'>
-              {matchedProfiles.map((item, index) => (
+            </Card>
+            <div className='d-flex flex-column side-menues-h justify-content-center align-items-start mb-2'>
+              {sideBarHomeMenues.map(item => (
                 <div
-                  onClick={() => navigate(`/profile/${item.id}`)}
-                  key={index.toString()}
-                  className='d-flex matches-h-wrapper col-md-2 flex-column justify-content-center align-items-start'
+                  onClick={() => navigate(item.path)}
+                  className={`d-flex ${
+                    pathname === item.path ? 'menu-container-active' : null
+                  } menu-container-h flex-row justify-content-start align-items-center`}
+                  key={item.title}
                 >
-                  <img className='matches-profile-img' src={nodeBaseURL + '/' + item.image} />
-                  <h5 className='recent-profile-c matches-name ml-1'>{item.name}</h5>
-                  <p className='matches-profile-c ml-1'>
-                    {item.points}%, Type: {item.personality_type}
-                  </p>
+                  {item.icon}
+                  <h6 className='menu-heading-h'>{item.title}</h6>
                 </div>
               ))}
             </div>
           </div>
+          <div className='d-flex flex-column col-md-9'>
+            <div className='d-flex flex-column new-matches-h p-4'>
+              <div className='d-flex flex-row align-items-center'>
+                <div className='d-flex flex-column'>
+                  <h5 className='recent-heading-h'>Matches</h5>
+                  <h5 className='new-match-heading-h'>
+                    PAAKMATCH, Found {matchedProfiles.length} Members{' '}
+                  </h5>
+                </div>
+              </div>
+              <div className='d-flex flex-row recent-h-container'>
+                {matchedProfiles.map((item, index) => (
+                  <div
+                    onClick={() => navigate(`/profile/${item.id}`)}
+                    key={index.toString()}
+                    className='d-flex matches-h-wrapper col-md-2 flex-column justify-content-center align-items-start'
+                  >
+                    <img className='matches-profile-img' src={nodeBaseURL + '/' + item.image} />
+                    <h5 className='recent-profile-c matches-name ml-1'>{item.name}</h5>
+                    <p className='matches-profile-c ml-1'>
+                      {item.points}%, Type: {item.personality_type}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
 
